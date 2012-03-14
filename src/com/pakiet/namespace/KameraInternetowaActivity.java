fd1c8c10cc3f525885d38ac91c85fee124bd42d8
Konrad Zapala <konri1990@gmail.com>
2012-03-14 20:55:16 +0100

    Zalozenie repozytorium i utworzenie projektu

---
+++ src/com/pakiet/namespace/KameraInternetowaActivity.java
@@ -0,0 +1,13 @@
+package com.pakiet.namespace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KameraInternetowaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
