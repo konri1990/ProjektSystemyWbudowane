8279ab3f4a9a93869b849e568f471f1613220188
Konrad Zapala <konri1990@gmail.com>
2012-03-14 21:59:50 +0100

    plan projektu + hello android

--- src/com/pakiet/namespace/KameraInternetowaActivity.java
+++ src/com/pakiet/namespace/KameraInternetowaActivity.java
@@ -2,12 +2,15 @@ package com.pakiet.namespace;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class KameraInternetowaActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
     }
 }
