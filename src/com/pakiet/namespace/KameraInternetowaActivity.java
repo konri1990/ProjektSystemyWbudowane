e5329d2d05a35a16ea17353568450b379d8fa97e
Konrad Zapala <konri1990@gmail.com>
2012-03-22 14:01:08 +0100

    Pole input + button wyslij na serwer(narazie tylko wyswietla text z inputa pod spodem)

--- src/com/pakiet/namespace/KameraInternetowaActivity.java
+++ src/com/pakiet/namespace/KameraInternetowaActivity.java
@@ -2,15 +2,35 @@ package com.pakiet.namespace;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class KameraInternetowaActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Android");
-        setContentView(tv);
-    }
+	/** Called when the activity is first created. */
+	EditText input;
+	TextView output;
+	Button buttonClick;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		input = (EditText) findViewById(R.id.EditText01);
+		output = (TextView) findViewById(R.id.TextView01);
+		buttonClick = (Button) findViewById(R.id.Button01);
+		buttonClick.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				display(input.getText());
+			}
+		});
+	}
+
+	protected void display(Editable text) {
+		output.setText(text);
+	}
 }
