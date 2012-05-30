b7b6f67eb3d9b98f453f01eb9d41f165657c7768
Tomek <tomaszziebiec@gmail.com>
2012-05-30 18:56:22 +0200

    camera activity

--- gen/com/pakiet2/namespace/R.java
+++ gen/com/pakiet2/namespace/R.java
@@ -17,6 +17,7 @@ public final class R {
         public static final int button1=0x7f050002;
         public static final int button2=0x7f050003;
         public static final int imageView1=0x7f050004;
+        public static final int surfaceView1=0x7f050005;
         public static final int textView1=0x7f050001;
         public static final int wifiInfo=0x7f050000;
     }
--- src/com/pakiet2/namespace/KameraInternetowaActivity.java
+++ src/com/pakiet2/namespace/KameraInternetowaActivity.java
@@ -1,16 +1,32 @@
 package com.pakiet2.namespace;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Timer;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.PictureCallback;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.provider.MediaStore.Images;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.SurfaceHolder.Callback;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,13 +35,26 @@ import android.widget.Toast;
 import android.widget.TextView;
 
 public class KameraInternetowaActivity extends Activity implements
-		OnClickListener {
+		OnClickListener, SurfaceHolder.Callback {
 	private Timer timer = new Timer();
 	private boolean clicked = false;
 	private MyServer Serwer;
 	private static final int CAMERA_REQUEST = 1888;
-    private ImageView iv;
+	Camera mCamera;
+	private ImageView iv;
 	Context mycontext;
+
+    //a variable to store a reference to the Surface View at the main.xml file 
+    private SurfaceView sv; 
+ 
+    //a bitmap to display the captured image 
+    private Bitmap bmp; 
+ 
+    //Camera variables 
+    //a surface holder 
+    private SurfaceHolder sHolder; 
+    //the camera parameters 
+    private Parameters parameters;  
 
 	// Thread t = null;
 	@Override
@@ -38,7 +67,7 @@ public class KameraInternetowaActivity extends Activity implements
 		TextView tv = (TextView) findViewById(R.id.textView1);
 		tv.setText("Kamera Internetowa wersja 1.0\n");
 		iv = (ImageView) findViewById(R.id.imageView1);
-		
+
 		ConnectivityManager conman = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		boolean wifi = conman.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
 				.isConnectedOrConnecting();
@@ -49,6 +78,19 @@ public class KameraInternetowaActivity extends Activity implements
 		} else {
 			wifi_info.setText("Your WI-FI is OFF");
 		}
+		
+		//get the Surface View at the main.xml file 
+        sv = (SurfaceView) findViewById(R.id.surfaceView1); 
+ 
+        //Get a surface 
+        sHolder = sv.getHolder(); 
+ 
+        //add the callback interface methods defined below as the Surface View callbacks 
+        sHolder.addCallback(this); 
+ 
+        //tells Android that this surface will have its data constantly replaced 
+        sHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS); 
+    
 
 		Button bt = (Button) findViewById(R.id.button1);
 		bt.setText("START");
@@ -99,25 +141,79 @@ public class KameraInternetowaActivity extends Activity implements
 			}
 		}
 		if (v.getId() == R.id.button2) {
-			Intent cameraIntent = new Intent(
-					android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-			startActivityForResult(cameraIntent, CAMERA_REQUEST);
+			// Intent cameraIntent = new Intent(
+			// android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			// startActivityForResult(cameraIntent, CAMERA_REQUEST);
+			
 		}
 	}
 
 	/* Cykanie zdjecia */
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	super.onActivityResult(requestCode, resultCode, data);
+		super.onActivityResult(requestCode, resultCode, data);
 		if (requestCode == CAMERA_REQUEST) {
 			if (resultCode == RESULT_OK) {
-		    	Bitmap bm = (Bitmap) data.getExtras().get("data");
-		    	iv.setImageBitmap(bm);
+				Bitmap bm = (Bitmap) data.getExtras().get("data");
+				iv.setImageBitmap(bm);
 			} else if (resultCode == RESULT_CANCELED) {
 				Toast.makeText(getApplicationContext(), "Cancelled",
 						Toast.LENGTH_SHORT).show();
 			}
 
 		}
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		  //get camera parameters 
+        parameters = mCamera.getParameters(); 
+
+        //set camera parameters 
+        mCamera.setParameters(parameters); 
+        mCamera.startPreview(); 
+
+        //sets what code should be executed after the picture is taken 
+        Camera.PictureCallback mCall = new Camera.PictureCallback() 
+        { 
+            @Override 
+            public void onPictureTaken(byte[] data, Camera camera) 
+            { 
+                //decode the data obtained by the camera into a Bitmap 
+                bmp = BitmapFactory.decodeByteArray(data, 0, data.length); 
+                //set the iv_image 
+                iv.setImageBitmap(bmp); 
+            } 
+        }; 
+
+        mCamera.takePicture(null, null, mCall);  
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// The Surface has been created, acquire the camera and tell it where 
+        // to draw the preview. 
+        mCamera = Camera.open(); 
+        try { 
+           mCamera.setPreviewDisplay(holder); 
+ 
+        } catch (IOException exception) { 
+            mCamera.release(); 
+            mCamera = null; 
+        }  
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		//stop the preview 
+        mCamera.stopPreview(); 
+        //release the camera 
+        mCamera.release(); 
+        //unbind the camera from this object 
+        mCamera = null;  
+		
 	}
 
 }
